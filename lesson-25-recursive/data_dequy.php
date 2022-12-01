167735d70c79c18cce8036ace567f4e9fcfbc48f
Luu_Khoa_Hoc <luatluukhoa@gmail.com>
2022-12-02 00:04:41 +0700

    last day 2 learn PHP

---
+++ lesson-25-recursive/data_dequy.php
@@ -0,0 +1,94 @@
+<?php 
+$data = array(
+    array(
+        "id" => 1,
+        "name" => "Trang Chủ",
+        "parent" => 0
+    ),
+    array(
+        "id" => 2,
+        "name" => "Giới Thiệu",
+        "parent" => 0
+    ),
+    array(
+        "id" => 3,
+        "name" => "Tin Tức",
+        "parent" => 0
+    ),
+    array(
+        "id" => 4,
+        "name" => "Sản Phẩm",
+        "parent" => 0
+    ),
+    array(
+        "id" => 5,
+        "name" => "Liên Hệ",
+        "parent" => 0
+    ),
+    array(
+        "id" => 6,
+        "name" => "Thế Giới",
+        "parent" => 3
+    ),
+    array(
+        "id" => 7,
+        "name" => "Xã Hội",
+        "parent" => 3
+    ),
+    array(
+        "id" => 8,
+        "name" => "Thời Trang",
+        "parent" => 3
+    ),
+    array(
+        "id" => 9,
+        "name" => "Cuộc Sống",
+        "parent" => 3
+    ),
+    array(
+        "id" => 10,
+        "name" => "Apple",
+        "parent" => 4
+    ),
+    array(
+        "id" => 11,
+        "name" => "Samsung",
+        "parent" => 4
+    ),
+    array(
+        "id" => 12,
+        "name" => "LG",
+        "parent" => 4
+    ),
+    array(
+        "id" => 13,
+        "name" => "Sony",
+        "parent" => 4
+    ),
+    array(
+        "id" => 14,
+        "name" => "Xiaomi",
+        "parent" => 4
+    ),
+    array(
+        "id" => 15,
+        "name" => "Oppo",
+        "parent" => 4
+    ),
+    array(
+        "id" => 16,
+        "name" => "iPhone 14",
+        "parent" => 10
+    ),
+    array(
+        "id" => 17,
+        "name" => "iPhone 13",
+        "parent" => 10
+    ),
+    array(
+        "id" => 18,
+        "name" => "iPhone 12",
+        "parent" => 10
+    ),
+);
+?>
---
+++ lesson-26-form/get.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+<form action="" method="get">
+    <table>
+        <tr>
+            <td>Name</td>
+            <td><input type="text" name="hoten" id=""></td>
+        </tr>
+        <tr>
+            <td>Gender</td>
+            <td><input type="radio" name="gender" value="0" checked >Nữ</td>
+            <td><input type="radio" name="gender" value="1" >Nam</td>
+        </tr>
+        <tr>
+            <td colspan="2"><input type="submit" name="send" value="Gửi" id="">
+            </td>
+        </tr>
+    </table>
+
+</form>
+
+</body>
+</html>
